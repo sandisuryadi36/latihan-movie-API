ad9a585a7436e8472addec6186b9b7a338465c9e
sandisuryadi36 <sandisuryadi.mail@gmail.com>
2021-12-23 10:21:12 +0700

    test

--- main.go
+++ main.go
@@ -14,10 +14,8 @@ import (
 // @contact.name API Support
 // @contact.url http://www.swagger.io/support
 // @contact.email support@swagger.io
-
 // @license.name Apache 2.0
 // @license.url http://www.apache.org/licenses/LICENSE-2.0.html
-
 // @termsOfService http://swagger.io/terms/
 
 func main() {
